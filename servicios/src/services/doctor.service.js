5ab8202c977916904985ce7cd2fd0e03692f67b9
unknown <miguelguevara_94@outlook.com>
2022-08-25 21:01:56 -0500

    obtener medicos por especialidad

--- servicios/src/services/doctor.service.js
+++ servicios/src/services/doctor.service.js
@@ -12,6 +12,14 @@ class DoctorService{
         return result;
     }
 
+    async getAllByEspecialidad(id_especialidad){
+        const sqlAll = `select * from Medico m inner join MedicoEspecialidad mes on (m.id=mes.id_medico)
+        inner join Especialidad e on (e.id=mes.id_especialidad) where e.id=${id_especialidad}`;
+        const result = await this.sql.querySql(sqlAll);
+        return result;
+    }
+    
+
 }
 
 module.exports = DoctorService;
